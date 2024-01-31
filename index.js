651f480c72aaec03509dea17002df74b90d3a00b
吴杰 <1220253798@qq.com>
2024-01-31 16:35:04 +0800

    v1.2.6

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
       {
         ignoreWhenNoAttributes: true,
         ignoreWhenEmpty: true,
-        externalIgnores: ['button'],
+        externalIgnores: ['button', 'option'],
       },
     ],
   },
